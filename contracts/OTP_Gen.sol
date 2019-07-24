bf678038a11c9a0ad89302d95082dce963271d40
Jack Boyuan Xu <boyuanxu@usc.edu>
2019-07-25 00:03:45 +0800

    Revised data structure

--- contracts/OTP_Gen.sol
+++ contracts/OTP_Gen.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.5.0;
 
 library OTP_Gen {
 
-    function generate(address dst, string memory seed) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(dst, seed));
+    function generate(address dst, string memory fiatSeed, string memory exchangeSeed) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(dst, fiatSeed, exchangeSeed));
     }
 
 }
--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -9,20 +9,17 @@ contract Remittance is Stoppable {
 
     using SafeMath for uint;
 
-    mapping (address => TxInfo[]) TxLUT; // Recipient address => TxInfo[]
-    event LogDepositComplete(address indexed sender, address indexed fiatRecipient, address indexed exchangeDst, uint amount, uint deadline);
-    event LogWithdrawPending(address indexed fiatRecipient, address indexed exchangeDst, uint amount);
-    event LogWithdrawComplete(address indexed fiatRecipient, address indexed exchangeDst, uint amount);
-    event LogReclaimComplete(address indexed sender, address indexed fiatRecipient, address indexed exchangeDst, uint amount, uint deadline);
+    mapping (bytes32 => TxInfo) TxLUT; // Hash => TxInfo
+    event LogDepositComplete(address indexed sender, address indexed dst, uint amount, uint deadline);
+    event LogWithdrawPending(address indexed sender, address indexed dst, uint amount);
+    event LogWithdrawComplete(address indexed sender, address indexed dst, uint amount);
+    event LogReclaimComplete(address indexed sender, address indexed dst, uint amount, uint deadline);
 
     struct TxInfo {
-        address sender;
-        address fiatRecipient;
+        address payable sender;
+        address payable dst;
         uint amount;
         uint deadline;
-        bytes32 fiatRecipientPwHash;
-        bytes32 exchangePwHash;
-        bool pendingAuthRelease;
     }
 
     constructor(bool initialRunState) public Stoppable(initialRunState) {}
@@ -32,94 +29,60 @@ contract Remittance is Stoppable {
         _;
     }
 
-    // Called by Alice
-    function deposit(address fiatRecipient, address exchangeDst, uint deadline, bytes32 fiatRecipientPwHash, bytes32 exchangePwHash)
-    public payable onlyIfRunning addressNonZero(exchangeDst) sufficientIncomingFunds returns(bool success) {
-        if (findTxInfoLoose(exchangeDst, fiatRecipientPwHash, exchangePwHash) != 2**256-1) {     // If tx already exists or password is already in use, abort.
+    modifier txExists(bytes32 pwHash) {
+        require(txExistsHelper(pwHash), "E_TNF");
+        _;
+    }
+
+    // Called by Alice.
+    function deposit(address payable dst, uint deadline, bytes32 pwHash)
+    public payable onlyIfRunning addressNonZero(dst) sufficientIncomingFunds returns(bool success) {
+        if (txExistsHelper(pwHash)) {     // If tx already exists or password is already in use, abort.
             revert("E_TAE");
         }
         if (deadline != 0 && deadline < block.number) {
             revert("E_DE");
         }
-        TxInfo memory t = TxInfo(msg.sender, fiatRecipient, msg.value, deadline, fiatRecipientPwHash, exchangePwHash, false);
-        TxLUT[exchangeDst].push(t);
-        emit LogDepositComplete(msg.sender, fiatRecipient, exchangeDst, msg.value, deadline);
+        TxInfo memory t = TxInfo(msg.sender, dst, msg.value, deadline);
+        TxLUT[pwHash] = t;
+        emit LogDepositComplete(msg.sender, dst, msg.value, deadline);
         return true;
     }
 
-    // Called by Bob and Carol, once per person.
-    function withdraw(address payable exchangeDst, bytes32 pwHash)
-    public onlyIfRunning returns (bool withdrawn) {
-        uint TxIndex = findTxInfoStrict(exchangeDst, pwHash);
-        if (TxIndex == 2**256-1) {       // If tx doesn't exist, abort.
-            revert("E_TNF");
+    // Called by Carol (with Bob).
+    // Use OTP_Gen to get hash.
+    function withdraw(bytes32 pwHash)
+    public onlyIfRunning txExists(pwHash) returns (bool success) {
+        TxInfo memory t = TxLUT[pwHash];
+        if (t.dst != msg.sender) {
+            revert("E_UA");
         }
-        TxInfo[] storage tArray = TxLUT[exchangeDst];
-        TxInfo memory t = tArray[TxIndex];
         if (t.deadline != 0 && (t.deadline <= block.number)) {      // If tx has expired, abort.
             revert("E_TE");
         }
-        uint amount = t.amount;
-        if (t.pendingAuthRelease) {       // If the other party has already authorized the release.
-            delete tArray[TxIndex];
-            emit LogWithdrawComplete(t.fiatRecipient, exchangeDst, amount);
-            exchangeDst.transfer(amount);
-            return true;
-        } else {
-            t.pendingAuthRelease = true;
-            tArray[TxIndex] = t;
-            emit LogWithdrawPending(t.fiatRecipient, exchangeDst, amount);
-            return false;
-        }
+        emit LogWithdrawComplete(t.sender, t.dst, t.amount);
+        t.dst.transfer(t.amount);
+        return true;
     }
 
-    // Only called by Alice to reclaim funds
-    function reclaim(address exchangeDst, bytes32 fiatRecipientPwHash, bytes32 exchangePwHash)
-    public onlyIfRunning addressNonZero(exchangeDst) returns (bool success) {
-        uint TxIndex = findTxInfoLoose(exchangeDst, fiatRecipientPwHash, exchangePwHash);
-        if (TxIndex == 2**256-1) {
-            revert("E_TNF");
-        }
-        TxInfo[] storage tArray = TxLUT[exchangeDst];
-        TxInfo memory t = tArray[TxIndex];
+    // Only called by Alice to reclaim funds.
+    function reclaim(address dst, bytes32 pwHash)
+    public onlyIfRunning txExists(pwHash) addressNonZero(dst) returns (bool success) {
+        TxInfo memory t = TxLUT[pwHash];
         if (t.sender != msg.sender) {
             revert("E_UA");
         }
         if ((t.deadline > block.number) || (t.deadline == 0)) {
             revert("E_TNE");
         }
-        uint amount = t.amount;
-        delete tArray[TxIndex];
-        emit LogReclaimComplete(msg.sender, t.fiatRecipient, exchangeDst, amount, t.deadline);
-        msg.sender.transfer(amount);
+        emit LogReclaimComplete(t.sender, t.dst, t.amount, t.deadline);
+        msg.sender.transfer(t.amount);
         return true;
     }
 
     // Helper function to find a Tx struct
-    function findTxInfoStrict(address TxLUTKey, bytes32 pwHash)
-    private view onlyIfRunning returns (uint index) {
-        TxInfo[] memory tArray = TxLUT[TxLUTKey];
-        for (uint i = 0; i < tArray.length; i++) {
-            TxInfo memory t = tArray[i];
-            // If caller is fiat recipient and the hash matches stored fiat hash 
-            // OR caller is exchange and hash matches stored exchange hash
-            if (((msg.sender == t.fiatRecipient) && (t.fiatRecipientPwHash == pwHash)) || ((msg.sender == TxLUTKey) && (t.exchangePwHash == pwHash))) {
-                return i;
-            }
-        }
-        return 2**256-1;
+    function txExistsHelper(bytes32 pwHash)
+    private view onlyIfRunning returns (bool) {
+        return ((TxLUT[pwHash].sender == address(0)) && (TxLUT[pwHash].dst == address(0)));
     }
-
-    function findTxInfoLoose(address TxLUTKey, bytes32 fiatRecipientPwHash, bytes32 exchangePwHash)
-    private view onlyIfRunning returns (uint index) {
-        TxInfo[] memory tArray = TxLUT[TxLUTKey];
-        for (uint i = 0; i < tArray.length; i++) {
-            TxInfo memory t = tArray[i];
-            if ((t.fiatRecipientPwHash == fiatRecipientPwHash) && (t.exchangePwHash == exchangePwHash)) {
-                return i;
-            }
-        }
-        return 2**256-1;
-    }
-
 }
